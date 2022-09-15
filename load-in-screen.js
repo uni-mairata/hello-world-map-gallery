551c5df59f46308be984074a07aae8d1cc7ef5de
Maika Hirata <57153217+uni-mairata@users.noreply.github.com>
2022-09-15 15:08:39 +0000

    Style edits

--- load-in-screen.js
+++ load-in-screen.js
@@ -3,12 +3,12 @@ $("body").append(
 );
 
 $(window).on("load", function() {
-  setTimeout(removeLoader, 500); //wait for page load
+  setTimeout(removeLoader, 400); //wait for page load
 });
 
 function removeLoader() {
   $("html,body").scrollTop(0);
-  $("#loadingDiv").fadeOut(500, function() {
+  $("#loadingDiv").fadeOut(400, function() {
     // fadeOut complete. Remove the loading div
     $("#loadingDiv").remove(); //makes page more lightweight
   });
